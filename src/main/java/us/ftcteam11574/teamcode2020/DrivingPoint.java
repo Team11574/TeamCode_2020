86ee603b828b8834af046a9197969150bb974ea1
Julianna Schneider <juliannaschneider@icloud.com>
2021-01-01 19:40:17 -0800

    Code from 12/31/20 session and past TeleOp tests

---
+++ src/main/java/us/ftcteam11574/teamcode2020/DrivingPoint.java
@@ -0,0 +1,15 @@
+package us.ftcteam11574.teamcode2020;
+
+class DrivingPoint {
+    private double x, y, time;
+    public DrivingPoint(double xval, double yval){
+        x = xval;
+        y = yval;
+    }
+    public double xval(){
+        return x;
+    }
+    public double yval(){
+        return y;
+    }
+}
