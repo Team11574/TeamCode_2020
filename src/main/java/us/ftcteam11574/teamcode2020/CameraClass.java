241f63cc315db60f31f9c3114d3e635af4599fbd
Caleb-Briggs1 <31353331+Caleb-Briggs1@users.noreply.github.com>
2021-02-07 15:10:18 -0800

    OpenCV changes
    Not of this code is tested, and might not build properly. It should, though, be mainly sufficient once the values are fine-tuned to work well with the rings. At the moment, this also computes the contours, which can be useful for debugging purposes. It all logs the frames to the camera stream, after the contour finding procedure.

--- src/main/java/us/ftcteam11574/teamcode2020/CameraClass.java
+++ src/main/java/us/ftcteam11574/teamcode2020/CameraClass.java
@@ -26,25 +26,56 @@ public class CameraClass extends OpenCvPipeline {
         //Contour: https://docs.opencv.org/3.4/da/d0c/tutorial_bounding_rects_circles.html
         //Create a bounding box baseed on tehe colors, and find the height of the bounding box
     //TODO: can rewrite everything into the YCrCB channel, to avoid light issues
-    //TODO: take either the average of all centers, to take teh average of centers times teh size of the bounding rectangle
+    //TODO: take either the average of all centers, to take teh average of centers times the size of the bounding rectangle
     //TODO: submat to take just certain parts
     //TODO: get locatino based on the image.  
+    
+    
+    //NOTE: all the current values used in this aren't tested. I haven't yet worked out the correct search area, or optimized parameters for the hue, saturation, and value
     public double currentTotalColor = 0;
     public double a = 0;
     public double b = 0;
     public double c = 0;
+    
+    public Scalar out_yellow = 0;
+    public Scalar out_yellow_small = 0;
+    
+    
+    
+    //search area for the the stacks. Looks at the full stack of up to 4 rings
+    Point left_search = new Point(
+                50,
+                50);
+    Point right_search =  new Point(
+                100,
+                100);
+    
+    //A second search can make it easier to see the difference between 0 or 1 items inside the stack
+    Point small_left_search = new Point(
+                50,
+                50);
+    Point small_right_search =  new Point(
+                100,
+                100);
+    
+    //Yellow Hue
+    public double Yhue = 15; //remember, only from 0-180 NOT 0-360
+    public double Ysat = 200; 
+    public double Yval = 100;
 
 
-    // Values for mask
+    // Values for mask, just yellow versus some mask
+    
+    public double[] thresh = new double[]{15,30,50};
 
-    public double hue_low = 0;
-    public double hue_high = 0;
+    public double hue_low = Yhue - thresh[0];
+    public double hue_high = Yhue + thresh[0];
 
-    public double sat_low = 0;
-    public double sat_high = 0;
+    public double sat_low = Ysat - thresh[1];
+    public double sat_high = Ysat + thresh[1];
 
-    public double value_low = 0;
-    public double value_high = 0;
+    public double value_low = Yval - thresh[2];
+    public double value_high = Yval + thresh[2];
 
     // Values for BLUR
     int sX = 7;
@@ -65,46 +96,41 @@ public class CameraClass extends OpenCvPipeline {
 
         Imgproc.cvtColor(input,mat,Imgproc.COLOR_RGB2HSV); //convert to HSV, and output as mat
 
-        applyBlur(mat);
+        applyBlur(mat); //apply a blur to make it easier to recognize contours
+        
+        mat = inRange(mat, hue_low,sat_low,value_low,hue_high,sat_high,value_high); //find the pixels within some range, and keep those. Returns a grey scale image
+
+        Mat stack_mat = mat.submat(new Rect(left_search, right_search)); //This gets a sub matrix within a certain range, which should be where the stack is. Taking the average of the 
+        //colors should give a good estimation of the number of items within the stack
+        
+        Mat zero_v_one_mat = mat.submat(new Rect(small_left_search, small_right_search)); //This gets the sub matrix of the bottom ring. 
+        
+        out_yellow = Core.mean(stack_mat); //Not entirely sure if this function is correctly, but it shoudl take the mean within that range. The lighter it is, the more yellow must of been there
+        out_yellow_small = Core.mean(zero_v_one_mat); // ^ same as above.
+        
+        
+        
+        
+        
+        
 
 
 
-
-        //Could write a simple thing to find the amount of yellow between some range, or whether yellow has a sufficently high value within some range
-
-        //note, hue range is normally from 0-360, but openCV keeps colors in the range of 0-180
-        //check if the color is in some range
-        //Mat res = inRange(mat, 00,40,100,50,160,250);
-
-        Mat rect = new Mat();
-
-        Point left = new Point(
-                50,
-                50);
-        Point right =  new Point(
-                100,
-                100);
-        //input.submat(new Rect(left, right));
-        //Imgproc.rectangle(input, left, right, new Scalar(60, 255, 255), 2);
-
-
-
-        //Scalar avg = Core.mean(rect);
-        //currentTotalColor = avg.val[2];
+        
 
 
 
 
 
 
-
-        //draw a rectangle
-        Object[] output = contours(findEdges(mat), mat); //destructive
-
-
+        
+        Object[] output = contours(findEdges(mat), mat); //destructive, creates the contours, and saves them to the Object class, this is mainly useful for seeing bounding
+        //boxes. This problably won't be all that useful later 
 
 
 
+
+        /*
         int row_s = 0;
         int row_e = 320;
         int col_s = 0;
@@ -113,7 +139,7 @@ public class CameraClass extends OpenCvPipeline {
          a = 0;
          b = 0;
         c = 0;
-        /*
+        
         for (int r = row_s;row_e > r;r++ ) {
             for (int cl = col_s;col_e > c;c++ ) {
                 a += mat.get(r,cl)[0];
@@ -127,26 +153,23 @@ public class CameraClass extends OpenCvPipeline {
         b /=  ( (row_e-row_s) * (col_e-col_s));
         c /=  ( (row_e-row_s) * (col_e-col_s));
         */
-
-
-
-        //Cb.submat(new Rect(region1_pointA, region1_pointB)); this is the region, can use submatrix to figure out the range
-
-
-
-        //Mat mat2 = new Mat();
-        //Imgproc.cvtColor(mat,mat2,Imgproc.COLOR_HSV2RGB);
+        
+        
         return mat;
 
     }
     public double returnVal() {
         return currentTotalColor;
     }
+    public Scalar[] returnYellows() {
+        return new Scalar[]{out_yellow, out_yellow_small};
+    }
     public Mat inRange(Mat mat, int h1, int s1,int v1, int h2, int s2, int v2) {
         Scalar clr1 = new Scalar(h1,s1,v1);
         Scalar clr2 = new Scalar(h2,s2,v2);
         Mat out = new Mat();
-        Core.inRange(mat, clr1, clr2, out ); //this makes all the true values black, and all the false values white, desecrtuctive on mat
+        Core.inRange(mat, clr1, clr2, out ); //this makes all the true values black, and all the false values white, desecrtuctive on mat. 
+        //Values that are close get lighter, and farther away values are darker
         return out;
         //example: https://stackoverflow.com/questions/36693348/java-opencv-core-inrange-input-parameters
     }
